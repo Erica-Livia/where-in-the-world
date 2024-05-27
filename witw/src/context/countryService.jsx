6e84ca4bc5fa168abb9044a93f1038cee78f9d39
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-27 20:58:10 +0200

    30sec

--- witw/src/context/countryService.jsx
+++ witw/src/context/countryService.jsx
@@ -4,7 +4,7 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 
 export const getAllCountries = async () => {
     try {
-        const response = await axios.get(`${BASE_URL}/all`, { timeout: 10000 });
+        const response = await axios.get(`${BASE_URL}/all`, { timeout: 30000 });
         return response.data;
     } catch (error) {
         console.error("Error fetching countries data: ", error);
